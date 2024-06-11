02434866622da6df75ce2374caaaf20d38e2d69a
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 19:21:24 +0530

    Navbar created and distributed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
 import View from './components/View';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <Add/>
-    <View/>
+
+      <Add />
+      <View />
+
     </div>
   );
 }
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const View = () => {
     return (
         <div>
-
+            <Navbar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -14,14 +15,14 @@ const View = () => {
                                 <tr>
                                     <th scope="col">Employee Id</th>
                                     <th scope="col">Employee Name</th>
-                                    
+
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
                                     <td>Mark</td>
-                                    
+
                                 </tr>
 
                             </tbody>
