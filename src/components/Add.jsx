9ac8fa06935400021e296acd6fa653742c2ef8e1
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 19:55:52 +0530

    inputs on the form printed on console

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Add = () => {
+
+    const [data, setData] = useState(
+        {
+            "empId": "",
+            "empName": ""
+        }
+    )
+
+    const inputHandler = (event) => {
+
+        setData({ ...data, [event.target.name]: event.target.value })
+
+    }
+
+
+    const readValue = () => {
+
+        console.log(data)
+
+    }
+
+
+
+
     return (
         <div>
             <Navbar />
@@ -15,19 +39,19 @@ const Add = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Employee Id</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='empId' value={data.empId} onChange={inputHandler}/>
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 
                                 <label htmlFor="" className="form-label">Employee Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='empName' value={data.empName} onChange={inputHandler} />
 
 
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                                <button className="btn btn-success">Add</button>
+                                <button className="btn btn-success" onClick={readValue}>Add</button>
 
 
                             </div>
