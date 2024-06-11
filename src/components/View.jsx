45e1165c7aebf9c82a959df4864ca492ff1a9abf
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 19:38:32 +0530

    Semi dynamic done

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const View = () => {
+
+    const [data, changeData] = useState(
+        [
+
+
+            {
+                "_id": "66682b0592aa58f41ebd6ace",
+                "empId": "205",
+                "empName": "Rosy",
+                "__v": 0
+            },
+            {
+                "_id": "666859bb7291258cdfed6a7a",
+                "empId": "201",
+                "empName": "Ria",
+                "__v": 0
+            }
+        ]
+
+    )
+
+
     return (
         <div>
             <Navbar />
@@ -12,18 +34,37 @@ const View = () => {
 
                         <table class="table table-dark table-striped">
                             <thead>
+
                                 <tr>
                                     <th scope="col">Employee Id</th>
                                     <th scope="col">Employee Name</th>
 
                                 </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
 
-                                </tr>
+                            </thead>
+
+                            <tbody>
+
+
+                                {data.map(
+
+                                    (value, index) => {
+
+                                        return <tr>
+                                            <th scope="row">{value.empId}</th>
+                                            <td>{value.empName}</td>
+
+                                        </tr>
+
+
+                                    }
+
+
+
+                                )}
+
+
+
 
                             </tbody>
                         </table>
